0744e41c808bcf24aa309a19f795500f1712d7df
Sebastián Mancilla <smancill.m@gmail.com>
2019-03-18 22:19:20 -0300

    Set 1 Challenge 8: Detect AES in ECB mode

---
+++ src/set01/challenge08.cpp
@@ -0,0 +1,58 @@
+#include <crypto.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <string>
+
+using crypto::bytes_t;
+using blocks_t = std::vector<bytes_t>;
+
+
+blocks_t split_into_blocks(const bytes_t& data, size_t block_size = 16)
+{
+    auto blocks = blocks_t{};
+    for (size_t i = 0; i < data.size() - block_size; i += block_size) {
+        blocks.emplace_back(data.begin() + i, data.begin() + i + block_size);
+    }
+    return blocks;
+}
+
+
+
+int count_repetitions(const blocks_t& blocks)
+{
+    auto unique_blocks = std::set<bytes_t>{blocks.begin(), blocks.end()};
+    return blocks.size() - unique_blocks.size();
+}
+
+
+int main(int argc, char** argv)
+{
+    if (argc != 2) {
+        std::cout << "usage: " << argv[0] << " <input_file>" << std::endl;
+        return 1;
+    }
+
+    struct {
+        int line = 0;
+        int reps = 0;
+    } text;
+
+    auto input = std::ifstream{argv[1]};
+    auto counter = 0;
+    for (std::string line; std::getline(input, line); ) {
+        ++counter;
+        auto data = crypto::hex2bytes(line);
+        auto reps = count_repetitions(split_into_blocks(data));
+        if (reps > text.reps) {
+            text.line = counter;
+            text.reps = reps;
+        }
+    }
+
+    std::cout << "Line: " << text.line << std::endl;
+    std::cout << "Repeated blocks: " << text.reps << std::endl;
+
+    return 0;
+}
